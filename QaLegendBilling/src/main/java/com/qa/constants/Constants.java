f004947337173889ea757e61cb6011426f304eaf
AmruthaWilson <amruthawilson04@gmail.com>
2023-09-19 15:17:09 +0530

    Com.qa.constants -->Constants.java

---
+++ QaLegendBilling/src/main/java/com/qa/constants/Constants.java
@@ -0,0 +1,13 @@
+package com.qa.constants;
+
+public class Constants 
+{
+	public static final String screenShot_path=System.getProperty("user.dir")+"\\Screenshots\\"; 
+	
+	public static final String testData=System.getProperty("user.dir")+"\\src\\test\\resources\\testData\\QaBillingLogin.xlsx";
+	
+	public static final String propertyConfig_File=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
+ 
+	//System.getProperty("user.dir")
+	//using this method we will get"C:\\Users\\hp\\eclipse-workspace\\QaLegendBilling" upto this
+}
